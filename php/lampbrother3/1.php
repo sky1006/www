d07488564e1e972c5b937250893f04a2dfbdab0b
sky1006 <yinqiang86106@163.com>
2014-08-18 18:11:32 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

---
+++ php/lampbrother3/1.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>我的第一个PHP页面</title>
+</head>
+<body>
+<h2>
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: Administrator
+     * Date: 14-8-18
+     * Time: 下午3:51
+     *
+     * 结构定义语句，后面一定不要加分号
+     */
+    //功能执行语句，后面必须加分号
+    echo("Hello,PHP!");
+    echo "aaaaaaaa";
+    //结构定义语句，后面一定不要加分号
+    if (true) {
+
+    }
+    while (true) {
+
+    }
+
+    ?>
+</h2>
+</body>
+
+
+</html>
---
+++ php/lampbrother3/2.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-8-18
+ * Time: 下午4:56
+ */
+echo("哈哈，妹子<br>");
+echo("123<br>");
+$str = "hello";
+?>
+
+<?
+echo("sfsgsd<br>");
+?>
+
+<?= $str ?>
+
+<%
+echo("yyyyy");
+%>
+
+    <script language="php">echo "uuuuu<br>";</script>
+
+<?php
+echo("hello world");
