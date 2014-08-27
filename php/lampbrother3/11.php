bbb6f70bc13ee3cf73301b05a6d6f630c345d4ea
sky1006 <yinqiang86106@163.com>
2014-08-27 17:59:38 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

--- php/lampbrother3/11.php
+++ php/lampbrother3/11.php
@@ -4,4 +4,57 @@
  * User: Administrator
  * Date: 14-8-25
  * Time: 下午5:02
- */ 
+ * 数组
+ */
+$arr = array(1, 2, 3, 4, 5);
+
+/*
+ * 对象
+ *
+ */
+
+class Person
+{
+    var $name;
+    var $age;
+    var $sex;
+
+    function say()
+    {
+
+    }
+
+    function eat()
+    {
+
+    }
+}
+
+$person = new Person();
+
+/*
+ * 资源
+ */
+$file = fopen("text.txt", "r");
+echo fread($file, 100);
+fclose($file);
+
+/*
+ * null = unset()
+ */
+
+/*
+ * 伪类型 number（说明一个参数可以是integer或float）
+ * mixed(说明一个参数可以接受多种不同的类型)
+ * callback(可以传函数)
+ *
+ * demo(mixed $a)  可以传任何类型的数据
+ * 功能：是两个数相加
+ * @param number $a 这是第一个数
+ * @
+ */
+function add($a, $b)
+{
+    return $a + $b;
+}
+
