c9f59d0e5c5709af6dab0befe304f1c53ac6c1e1
sky1006 <yinqiang86106@163.com>
2014-09-05 18:12:13 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

---
+++ php/lampbrother4/1.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-9-3
+ * Time: 下午5:31
+ * 顺序结构 -- 从上到下
+ *
+ * 分支结构：
+ *  单一条件分支结构（if）
+ *  双向条件分支结构（else从句）
+ *  多向条件分支结构（elseif子句，switch语句）
+ *  巢状条件分支结构
+ */ 
