56faaf0fd5a737cca0582cbaba47abd4a5851f13
sky1006 <yinqiang86106@163.com>
2014-09-23 18:04:28 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

--- php/lampbrother4/cal.php
+++ php/lampbrother4/cal.php
@@ -1,3 +1,63 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-9-22
+ * Time: 下午5:47
+ */
+if (isset($_POST['sub'])) {
+    // echo "用户提交了";
+
+    $bz = true;
+    $errormess = "有以上问题：<br>";
+    if ($_POST['num1'] == "") {
+        $bz = false;
+        $errormess .= "第一个数不能为空<br>";
+    }
+
+    if ($_POST['num2'] == "") {
+        $bz = false;
+        $errormess .= "第二个数不能为空<br>";
+    }
+
+    if (!is_numeric($_POST['num1'])) {
+        $bz = false;
+        $errormess .= "第一个不是数字不能计算<br>";
+    }
+    if (!is_numeric($_POST['num2'])) {
+        $bz = false;
+        $errormess .= "第二个不是数字不能计算<br>";
+    }
+
+    if ($bz) {
+        // 计算后的结果
+        $sum = "";
+        // 判断用户选择的是那个运算符合
+        switch ($_POST['ysf']) {
+            case '+':
+                $sum = $_POST['num1'] + $_POST['num2'];
+                break;
+            case '-':
+                $sum = $_POST['num1'] - $_POST['num2'];
+                break;
+            case '*':
+                $sum = $_POST['num1'] * $_POST['num2'];
+                break;
+            case '/':
+                $sum = $_POST['num1'] / $_POST['num2'];
+                break;
+            case '%':
+                $sum = $_POST['num1'] % $_POST['num2'];
+                break;
+        }
+    }
+}
+
+echo '<br>';
+//var_dump($_POST);
+?>
+
 <html>
 <head>
     <title>简单计算器</title>
@@ -27,21 +87,18 @@
                 <input type="submit" name="sub" value="计算">
             </td>
         </tr>
+        <tr>
+            <td colspan="4">
+                <?php
+                if ($bz) {
+                    echo "计算结果：{$_POST['num1']} {$_POST['ysf']} {$_POST['num2']} =$sum";
+                } else {
+                    echo $errormess;
+                }
+                ?>
+            </td>
+        </tr>
     </form>
 </table>
 </body>
 </html>
-<?php
-/**
- * Created by PhpStorm.
- * User: Administrator
- * Date: 14-9-22
- * Time: 下午5:47
- */
-if (isset($_POST['sub'])) {
-    echo "用户提交了";
-} else {
-    echo "用户是在刷新";
-}
-echo '<br>';
-//var_dump($_POST);
