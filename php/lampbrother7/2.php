13ba4c2c8ad6b4579a138ead31b01d79b95e9f8f
sky1006 <yinqiang86106@163.com>
2014-11-12 18:04:15 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

--- php/lampbrother7/2.php
+++ php/lampbrother7/2.php
@@ -4,4 +4,4 @@
  * User: Administrator
  * Date: 14-10-20
  * Time: 下午4:02
- */ 
+ */
--- php/lampbrother7/boyFriend.php
+++ php/lampbrother7/boyFriend.php
@@ -16,35 +16,64 @@
  *
  * 注意：在类的成员属性前面一定要有个修饰词，如果不知道使用什么修饰词，就用var（关键字），如果一旦
  * 有其他的修饰词就不要有var
+ *
+ *      只要是对象中的成员，就必须使用这个对象来访问到这个对象内部的属性和方法
+ *
+ * 构造方法
+ * 1、是对象创建完成以后，第一个自动调用的方法（特殊）
+ * 2、方法名称比较特殊，可以和类名相同名的方法名
+ * 3、给对象中的成员赋初值使用的
+ *
+ * 魔术方法 __construct();
+ *
+ * 析构函数是PHP5新添加的内容，在对象被销毁之前自动调用的方法，主要执行一些特定的操作，如关闭文件，释放结果集
+ *      __destruct() -- 不能带任何参数
+ *
  */
 class BoyFriend
 {
 //标记属性--变量（成员属性）
-    var $name = "yinqiang";
-    var $age = 24;
-    var $sex = "男";
-    var $height = "170cm";
-    var $eye = "big";
+    var $name;
+    var $age;
+    var $sex;
+    var $height;
+    var $eye;
 
+/*    function BoyFriend($name,$age,$sex="男") {
+        $this->name=$name;
+        $this->age=$age;
+        $this->sex=$sex;
+    }*/
+
+    function __construct($name,$age,$sex="男") {
+        $this->name=$name;
+        $this->age=$age;
+        $this->sex=$sex;
+    }
 //标记行为--函数（成员方法）
-    function doFan($rou, $cai)
+    function doFan()
     {
-        return "做饭功能<br>";
+        echo "{$this->name} 有做饭功能<br>";
+        $this->doJw();
     }
 
     function doJw()
     {
-        return "做家务功能<br>";
+        echo "做家务功能<br>";
     }
 
+    function __destruct() {
+        echo "{$this->name} 再见！<br>";
+    }
 }
 
-$bf1 = new BoyFriend();
-$bf1 -> name= "张三";
-$bf2 = new BoyFriend();
-$bf2->name= "李四";
-echo $bf1->sex . '<br>';
-echo $bf2->height.'<br>';
-echo $bf1->name . '<br>';
-
-echo $bf1->doFan("牛肉","土豆");
+$bf1 = new BoyFriend("小燕子",28);
+$bf2 = new BoyFriend("笨蛋",29,"男");
+//$bf1 -> name= "张三";
+//$bf2->name= "李四";
+echo $bf1 -> doFan();
+echo $bf2 -> doFan();
+//echo $bf1->sex . '<br>';
+//echo $bf2->height.'<br>';
+//echo $bf1->name . '<br>';
+//$bf1 = null;
