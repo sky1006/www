57d8558d546b095ac24290aae6045bf2c4604049
sky1006 <yinqiang86106@163.com>
2014-11-26 17:44:38 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

--- php/lampbrother7/6.php
+++ php/lampbrother7/6.php
@@ -87,5 +87,91 @@ $s1->say();
 $s2 = new StudentEn();
 $s2->say();
 
+/*
+ * 抽象类是一种抽象的类，接口是一种特殊的抽象类，接口也是一种特殊特殊的类
+ *      1、抽象类和接口中都有抽象方法；
+ *      2、抽象类和接口都不能创建实例对象；
+ *      3、抽象类和接口的使用意义也就是作用相同
+ *
+ * 接口和抽象类相比，特殊在哪里？
+ *      1、接口中的方法，必须全要素抽象方法（不能用不抽象的方法）,所以在接口中的抽象方法不需要abstract
+ *              直接使用分号结束即可
+ *      2、接口中的成员属性，必须是常量（不能有变量）
+ *      3、所有的权限必须是公有的（public）
+ *      4、声明接口不使用class，而是使用interface
+ */
 
-//$p = new Person();
+//声明一个接口使用interface
+interface Demo2
+{
+    const NAME = "燕子";
+    const AGE = 29;
+
+    function test();
+
+    function test2();
+
+    function test3();
+}
+
+echo Demo2::NAME;
+echo '<br>';
+
+/*
+ * 接口应用的一些细节:
+ *      1、可以使用extends 让一个接口继承另一个接口（接口和接口---只有扩展新抽象方法，没有覆盖的关系）
+ *      2、可以使用一个类来实现接口中的全部方法，也可以使用一个抽象类，来实现接口中的部分方法
+ *          （类与接口，抽象类与接口 -- 覆盖-- 重写，实现接口中的抽象方法）
+ *      3、就不要使用extends这个关键字，使用implements实现   implements相当于extends
+ *      extends 继承（扩展），这个在PHP中，一个类只能有一个父类
+ *      4、一个类可以在继承另一个类的同时，使用implements实现一个接口，可以实现多个接口（一定要先继承再实现接口）
+ *      5、实现多个接口，只需要使用逗号分开多个接口即可
+ */
+
+interface Demo3 extends Demo2
+{
+    function test4();
+}
+
+class World
+{
+    function test5()
+    {
+
+    }
+
+}
+
+interface Demo4 {
+    function test6() ;
+}
+class Hello extends World implements Demo3,Demo4
+{
+
+    function test()
+    {
+        // TODO: Implement test() method.
+    }
+
+    function test2()
+    {
+        echo "2222222222222<br>";
+    }
+
+    function test3()
+    {
+        // TODO: Implement test3() method.
+    }
+
+    function test4()
+    {
+        // TODO: Implement test4() method.
+    }
+
+    function test6()
+    {
+        // TODO: Implement test6() method.
+    }
+}
+$h =new Hello();
+$h->test2();
