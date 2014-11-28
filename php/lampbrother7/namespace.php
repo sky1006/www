506b8d25f3bb0db44f9d1251580fa3d5dac91059
sky1006 <yinqiang86106@163.com>
2014-11-28 18:13:04 +0800

    Revert "1"
    This reverts commit 8ceeb2ed78f91d1db80f6a36c8e56d4baa99cc62.
    
    Conflicts:
    	php/.idea/vcs.xml
    	php/.idea/workspace.xml
    	php/two/1.html

---
+++ php/lampbrother7/namespace.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-11-28
+ * Time: 下午4:58
+ * 命名空间 -- 名字空间  PHP5.3后引入
+ *      1、常量
+ *      2、函数
+ *      3、类
+ *使用namespace来声明；在namespace声明命名空间的代码上面，不能有任何PHP代码和HTML内容输出（除了declare），
+ * 声明命名空间只能是第一条
+ */
+//声明一个名字空间，前缀为yanzi
+//declare(encoding='utf-8');
+namespace yanzi;
+//定义子命名空间
+namespace yanzi\sub;
+const AAA=1;
+class Demo {
+    static function one() {
+        echo "11111111<br>";
+    }
+}
+function test() {
+    echo '2222222222<br>';
+}
+
+test();
+\yanzi\sub\test();
+echo AAA.'<br>';
+echo \yanzi\AAA.'<br>';
+
+Demo::one();
+\yanzi\Demo::one();
